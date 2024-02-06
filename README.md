5dc6c7f415ad1432bfb9960edd938c35088b993d
Nutchanon Ninyawee <nutchanon@codustry.com>
2024-02-06 23:29:46 +0700

    🌟ADD: Prototype

---
+++ README.md
@@ -0,0 +1,62 @@
+# GeoJSONchemy 🌍🧪
+GeoJSONchemy is a Python library that provides support for GeoJSON spatial data types in SQLAlchemy and SQLModel. It allows you to easily work with GeoJSON data in your database models. It currently supports ONLY PostgreSQL-[PostGIS](https://postgis.net/).
+
+![Cover Image Gen via MS Copilot Pro, a globe with map creator tools](cover.jpeg)
+
+## Installation 📦
+You can install GeoJSONchemy using pip:
+```bash
+pip install geojsonchemy
+
+```
+## Usage 🚀
+SQLAlchemy
+In SQLAlchemy, you can use the `GeomJSON` and `Geomdantic` classes from GeoJSONchemy as types for your model fields. Both are subclasses from `geoalchemy2.types.Geometry` 
+Here's an example:
+
+```python
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from geojsonchemy import GeomJSON, Geomdantic
+from geojson-pydantic import FeatureCollection
+
+Base = declarative_base()
+
+class FooModel(Base):
+    __tablename__ = 'table_name'
+
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    geom: Mapped[dict] = mapped_column(GeomJSON(geometry_type="POINT", srid=4326), nullable=False, index=True)
+    geom2: Mapped[FeatureCollection] = mapped_column(Geomdantic(geometry_type="GEOMETRY", srid=4326), nullable=False, index=True)
+```
+
+## SQLModel
+In [SQLModel](https://github.com/tiangolo/sqlmodel), you can use the `GeomJSON` and `Geomdantic` classes in a similar way:
+    
+```python   
+from sqlmodel import Field, SQLModel
+
+class GeomTable(SQLModel, table=True):
+    __tablename__ = "table_name"
+
+    id: int = Field(primary_key=True)
+    name: str
+    geom: dict = Field(
+        sa_type=GeomJSON(geometry_type="GEOMETRY", srid=4326),
+        nullable=False,
+    )
+    geom2: FeatureCollection = Field(
+        sa_type=Geomdantic(geometry_type="GEOMETRY", srid=4326),
+        nullable=False,
+    )
+
+```
+
+## Note 📝
+Please note that GeoJSONchemy currently only supports PostgreSQL. If you try to use it with a different database, it will raise a NotImplementedError.
+
+## Contributing 🤝
+Contributions are welcome! Please feel free to submit a pull request.
+
+## License 📄
+GeoJSONchemy is licensed under the MIT license. See the LICENSE file for more details.
---
+++ geojsonchemy/__init__.py
@@ -0,0 +1,70 @@
+import orjson
+from geoalchemy2.types import _GISType
+from sqlalchemy.sql import func
+from geojson_pydantic import Feature, FeatureCollection, Point
+from typing import Any, Callable, Optional
+import orjson
+from geoalchemy2.types import _GISType
+from sqlalchemy.sql import func
+from geojson_pydantic import Feature, FeatureCollection, Point
+
+class GeomJSON(_GISType):
+  """
+  Represents a spatial data type for storing as Geometry and manipulating geometries in GeoJSON format.
+
+  Attributes:
+    name (str): The name of the spatial data type.
+    from_text (str): The function used to convert a GeoJSON string to a geometry object.
+    as_binary (str): The function used to convert a geometry object to a GeoJSON string.
+    ElementType (Any): The type of the elements in the geometry object.
+    cache_ok (bool): Indicates whether the geometry object can be cached.
+  """
+
+  name: str = "GEOMETRY"
+  from_text: str = "ST_GeomFromGeoJSON"
+  as_binary: str = "ST_AsGeoJSON"
+  ElementType: Any = dict
+  cache_ok: bool = False
+
+  def column_expression(self, col: Any) -> Any:
+    return func.ST_AsGeoJSON(col, type_=self)
+
+  def bind_processor(self, dialect: Any) -> Callable[[Optional[Any]], str]:
+    """Specific bind_processor that automatically process spatial elements."""
+
+    if dialect.name != "postgresql":
+      raise NotImplementedError("Only PostgreSQL is supported")
+
+    return lambda geojson: orjson.dumps(geojson).decode('utf-8')
+
+  def result_processor(self, dialect: Any, coltype: Any) -> Callable[[Optional[str]], Any]:
+    def process(value: Optional[str]) -> Any:
+      if value is not None:
+        return orjson.loads(value)
+
+    return process
+
+class Geomdantic(_GISType):
+  name: str = "GEOMETRY"
+  from_text: str = "ST_GeomFromGeoJSON"
+  as_binary: str = "ST_AsGeoJSON"
+  ElementType: Any = FeatureCollection
+  cache_ok: bool = False
+
+  def column_expression(self, col: Any) -> Any:
+    return func.ST_AsGeoJSON(col, type_=self)
+
+  def bind_processor(self, dialect: Any) -> Callable[[Optional[Any]], str]:
+    """Specific bind_processor that automatically process spatial elements."""
+
+    if dialect.name != "postgresql":
+      raise NotImplementedError("Only PostgreSQL is supported")
+
+    return lambda geojson: orjson.dumps(geojson).decode('utf-8')
+
+  def result_processor(self, dialect: Any, coltype: Any) -> Callable[[Optional[str]], Any]:
+    def process(value: Optional[str]) -> Any:
+      if value is not None:
+        return orjson.loads(value)
+
+    return process
